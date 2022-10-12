8bc07749c3e1fc011ce1665bae8b48f51c7eae52
Marius <marius@example.com>
2022-10-12 10:50:35 +0300

    ter pradzia

---
+++ js/terenary.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log('terenary.js');
+
+let sentence = '';
+
+/*  
+
+sukurti kintamji isStudent (boolean), sukurti kintamaji Name - irasom varda. sukurti town ir irasysim miesta. 
+
+norim sugeneruoti sakini kintamajame sentence. 
+<vardas> gyvena <miestas> ir studijuoja/ ir dirba
+James gyvena London ir studijuoja
+Mike gyvena Kaunas ir dirba
+*/
+
+console.log(sentence);
