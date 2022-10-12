e66da39a73f78e5a396c7de6088445c54066b817
Marius <marius@example.com>
2022-10-12 09:54:09 +0300

    laikas

---
+++ js/laikas.js
@@ -0,0 +1,19 @@
+'use strict';
+console.log('laikas.js');
+
+/*   
+6 - 12 = rytas 
+13 - 18 = diena
+19 - 23 = vakaras
+24 - 5 = naktis
+
+ 
+*/
+
+let laikas = 13;
+
+if (laikas >= 6 && laikas <= 12) {
+  console.log('labas rytas');
+} else if () {
+  console.log('laba diena');
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 'use strict';
 console.log('main.js');
+
+// logical operators
+
+//  && - IR operatorius
+console.group('&& - IR operatorius');
+console.log('true && true ===', true && true);
+console.log('true && false ===', true && false);
+console.log('false && true ===', false && true);
+console.log('false && false ===', false && false);
+console.groupEnd();
+
+// || - ARBA operatorius
+console.group('|| - ARBA operatorius');
+console.log('true || true ===', true || true);
+console.log('true || false ===', true || false);
+console.log('false || true ===', false || true);
+console.log('false || false ===', false || false);
+console.groupEnd();
+
+console.log('!true', !true);
+console.log('!false', !false);
+console.log('!kazkas', !'kazkas');
+// !! - bang bang ~ Boolean('kazkas')
+console.log("!!'kazkas'", !!'kazkas');
+console.log('!!""', !!'');
